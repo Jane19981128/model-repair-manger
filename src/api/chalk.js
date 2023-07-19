3520d48342ef9e007068a65cd123d84f3354218c
高金盟 <1245973455@qq.com>
2023-07-19 10:36:18 +0800

    [ADDED]: http headers

--- src/api/chalk.js
+++ src/api/chalk.js
@@ -1,19 +1,33 @@
 import httpRequest from '../request/index';
+import { store, useUserStore } from '../store/store';
+import { storeToRefs } from "pinia";
+
+const userStore = useUserStore(store);
+
+const { getToken } = userStore;
+// const { token } = storeToRefs(userStore);
 
 export function apiChalkList(queryParams) {
+
     return httpRequest({
         method: 'get',
         url: `/manage/chalk`,
         params: {
             ...queryParams
         },
+        headers: {
+            Authorization: getToken().value
+        }
     });
 };
 
 export function apiChalkRecord(chalkid) {
     return httpRequest({
         method: 'get',
-        url: `/manage/chalk/${chalkid}`
+        url: `/manage/chalk/${chalkid}`,
+        headers: {
+            Authorization: getToken().value
+        }
     });
 };
 
@@ -21,13 +35,19 @@ export function apiRecordDelete(queryParams) {
     const { chalkid, recordId } = queryParams;
     return httpRequest({
         method: 'delete',
-        url: `/manage/chalk/${chalkid}/record/${recordId}`
+        url: `/manage/chalk/${chalkid}/record/${recordId}`,
+        headers: {
+            Authorization: getToken().value
+        }
     });
 };
 
 export function apiDrawingData(chalkid) {
     return httpRequest({
         method: 'get',
-        url: `/manage/chalk/${chalkid}/drawing`
+        url: `/manage/chalk/${chalkid}/drawing`,
+        headers: {
+            Authorization: getToken().value
+        }
     });
 };
