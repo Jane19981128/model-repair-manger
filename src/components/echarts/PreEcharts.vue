385ca718aae9504b66388a6384f4ec41c7d432aa
高金盟 <1245973455@qq.com>
2023-07-31 15:21:33 +0800

    [ADDED] backgroud image

--- src/components/echarts/PreEcharts.vue
+++ src/components/echarts/PreEcharts.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="myEchart" :style="{ 'background-image': `url(${bgImage})` }" ref="chalkEcharts">
+    <div class="myEchart" :style="{ 'height': `${imageHeight}px`, 'background-image': `url(${bgImage})` }"
+        ref="chalkEcharts">
     </div>
 </template>
 <script setup>
-import { ref, reactive, onMounted, watch } from 'vue';
+import { ref, reactive, onMounted, watch, nextTick } from 'vue';
 import * as echarts from 'echarts';
 
 const props = defineProps({
@@ -37,25 +38,59 @@ const formatOptions = (replaceMerge = [], options = {}) => {
  */
 
 let bgImage = ref('');
+let myEchart;
+const repairType = new Map([
+    ['trim', '裁剪'], ['window', '窗'], ['mirror', '镜子'], ['wall', '补墙']
+]);
 
 onMounted(() => {
-    const curFloorChalk = getCurFloorDrawing(0)[0];
-    initEchart(curFloorChalk);
+    myEchart = initEchart();
+    changeEchart(myEchart);
 });
 
 watch(() => props.data, () => {
-    const [curFloorChalk, legend] = getCurFloorDrawing(0);
-    const optionsOther = formatOptions(['legend']);
-
-    chalkEchart.setOption(
-        { series: curFloorChalk, legend: { data: legend } },
-        optionsOther
-    );
+    changeEchart(myEchart);
 });
 
 watch(() => props.bgImageList, (newVal) => {
     bgImage.value = props.bgImageList[0];
+    setBgImageSize(bgImage.value);
+
 });
+
+const changeEchart = (echartsInstance) => {
+    const [curFloorChalk, legend] = getCurFloorDrawing(0);
+    const [edgeMax, edgeMin] = edgeValue(curFloorChalk);
+    const optionsOther = formatOptions(['legend', 'xAxis', 'yAxis']);
+
+    const axis = {
+        max: edgeMax,
+        min: edgeMin
+    };
+
+    echartsInstance.setOption(
+        {
+            series: curFloorChalk,
+            legend: { data: legend },
+            xAxis: axis,
+            yAxis: axis
+        },
+        optionsOther);
+
+};
+
+let imageHeight = ref(500);
+const setBgImageSize = (url) => {
+    var img = new Image();
+    img.src = url;
+    img.onload = () => {
+        imageHeight.value = (img.height / img.width) * 520 + 20;
+
+        nextTick(() => {
+            myEchart.resize();
+        });
+    };
+};
 
 const getCurFloorDrawing = (i) => {
     const chalkList = props.data[i]?.drawing?.chalkList;
@@ -66,11 +101,13 @@ const getCurFloorDrawing = (i) => {
                 return [line[0] / 1000, line[1] / 1000];
             });
 
-            legend.push(item.type);
+            const name = repairType.get(item.type);
+            legend.push(name);
+
             const options = {
-                name: item.type,
-                symbolSize: 15,
-                symbol: 'triangle',
+                name,
+                symbolSize: 10,
+                // symbol: 'triangle',
                 // endLabel: {
                 //     show: true,
                 //     formatter: function (params) {
@@ -88,27 +125,43 @@ const getCurFloorDrawing = (i) => {
     return [];
 };
 
+const edgeValue = (array) => {
+    const newArray = [];
+
+    array.forEach(item => {
+        if (Array.isArray(item.data)) {
+            const data = item.data;
+
+            data.forEach(point => {
+                newArray.push(...point);
+            });
+        }
+
+    });
+
+    return [Math.max(...newArray), Math.min(...newArray)];
+};
+
+
 /**
  * 初始化echarts图标
  */
 const chalkEcharts = ref(null);
-let chalkEchart;
-const initEchart = (chalkData) => {
-    chalkEchart = echarts.init(chalkEcharts.value);
+const initEchart = () => {
+
+    let chalkEchart = echarts.init(chalkEcharts.value);
+
     let option = {
         grid: {
-            left: '10px',
-            right: '10px',
-            bottom: '10px',
-            top: '50px',
-            containLabel: true
+            left: '0px',
+            right: '0px',
+            bottom: '0px',
+            top: '20px',
+            containLabel: false
         },
-        legend: {
-            data: chalkData.map(item => {
-                return item.name;
-            })
-            // data: ['窗户', '剪刀', '墙', '切割']
-        },
+        // legend: {
+        //     data: repairType
+        // },
         xAxis: {
             min: -10,
             max: 10
@@ -116,18 +169,22 @@ const initEchart = (chalkData) => {
         yAxis: {
             min: -10,
             max: 10
-        },
-        series: chalkData,
+        }
     };
 
     chalkEchart.setOption(option);
+
+    return chalkEchart;
 };
 
 
 </script>
 <style scoped lang="scss">
 .myEchart {
-    width: 520px;
-    height: 500px;
+    width: 100%;
+    // background-color: aqua;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: 50% calc(50% + 10px);
 }
 </style>
